45528fa91ee800bada6c62e68f82778f963c4be1
BlackBerryID <iddf1994one@gmail.com>
2022-05-06 21:24:17 +0300

    refactor: add file constants. Create constant 'Paths' for router. Correct switch.tsx (prettier-error)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Welcome from './Modules/Pages/Welcome';
 import Template from './Modules/Template';
+import { Login } from './Modules/Pages/login';
+import { Paths } from './constants';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Template />}>
+        <Route path={Paths.home} element={<Template />}>
           <Route index element={<Welcome />} />
+          <Route path={Paths.login} element={<Login />} />
         </Route>
       </Routes>
     </Router>
