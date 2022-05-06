b3c9529da1ad445ec2b999de6ea518804a177835
BlackBerryID <iddf1994one@gmail.com>
2022-05-06 22:45:04 +0300

    feat: add login-form validation via "yup" library

--- src/Modules/Pages/login/index.tsx
+++ src/Modules/Pages/login/index.tsx
@@ -1,19 +1,36 @@
 import { Container, TextField, Button } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import './login.scss';
+import { useEffect } from 'react';
+
+const schema = yup.object().shape({
+  login: yup.string().email().required(),
+  password: yup.string().min(8).max(15).required(),
+});
 
 export const Login = () => {
-  const { handleSubmit, control } = useForm<FormValues>({
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>({
     defaultValues: {
       login: '',
       password: '',
     },
+    resolver: yupResolver(schema),
   });
 
   const onSubmit = (data: FormValues) => {
     console.log(data);
   };
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
 
   return (
     <Container maxWidth="xl">
@@ -21,13 +38,27 @@ export const Login = () => {
         <Controller
           name="login"
           control={control}
-          render={({ field }) => <TextField {...field} label="Email address" />}
+          render={({ field, formState }) => (
+            <TextField
+              {...field}
+              label="Email address"
+              error={!!formState.errors.login}
+              helperText={errors.login?.message ?? ''}
+            />
+          )}
         />
         <Controller
           name="password"
           control={control}
-          render={({ field }) => (
-            <TextField {...field} label="Password" type="password" sx={{ m: '40px 0' }} />
+          render={({ field, formState }) => (
+            <TextField
+              {...field}
+              label="Password"
+              type="password"
+              sx={{ m: '30px 0' }}
+              error={!!formState.errors.password}
+              helperText={errors.password?.message ?? ''}
+            />
           )}
         />
         <Button variant="contained" type="submit">
