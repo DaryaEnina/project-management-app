cdc9ad6c10027350f4a9720df77a911ac31c2780
BlackBerryID <iddf1994one@gmail.com>
2022-05-06 22:11:28 +0300

    feat: add styles for login-form

--- src/Modules/Pages/login/index.tsx
+++ src/Modules/Pages/login/index.tsx
@@ -1,6 +1,8 @@
 import { Container, TextField, Button } from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
 
+import './login.scss';
+
 export const Login = () => {
   const { handleSubmit, control } = useForm<FormValues>({
     defaultValues: {
@@ -15,7 +17,7 @@ export const Login = () => {
 
   return (
     <Container maxWidth="xl">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <Controller
           name="login"
           control={control}
@@ -24,7 +26,9 @@ export const Login = () => {
         <Controller
           name="password"
           control={control}
-          render={({ field }) => <TextField {...field} label="Password" type="password" />}
+          render={({ field }) => (
+            <TextField {...field} label="Password" type="password" sx={{ m: '40px 0' }} />
+          )}
         />
         <Button variant="contained" type="submit">
           Log In
