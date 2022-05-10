e480b2fb0d7b5a8eff5e4e830ee1657c0be345a9
Gothica <semidevilish666@gmail.com>
2022-05-10 15:01:03 +0300

    feat: welcome main info start design

--- src/Modules/Pages/Welcome/index.tsx
+++ src/Modules/Pages/Welcome/index.tsx
@@ -1,9 +1,31 @@
-function Welcome() {
+import './welcome.scss';
+
+const Welcome = () => {
   return (
-    <>
-      <div>Welcome page</div>
-    </>
+    <div className="welcome">
+      <div className="welcome__wrapper main">
+        <div className="welcome__info">
+          <h2 className="welcome__info--title">Project Management App</h2>
+          <p className="welcome__info--text">
+            Project Management App - a training project to create an application that helps an
+            individual in a team or group of developers achieve their goals.
+          </p>
+          <h3 className="welcome__info--subtitle">RS School. Курс React.</h3>
+          <p className="welcome__info--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nihil consequuntur
+            amet repudiandae iusto atque dignissimos? Reprehenderit dolor, placeat tenetur
+            consequuntur, alias laborum odio voluptas hic unde asperiores repudiandae culpa.
+          </p>
+        </div>
+        <div className="welcome__img"></div>
+      </div>
+      <div className="welcome__wrapper team">
+        <div className="welcome__video">
+          <h2>Presentation</h2>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Welcome;
