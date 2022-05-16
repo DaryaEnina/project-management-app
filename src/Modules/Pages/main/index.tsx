da02a81839ee17269760682a77ba1a079d278729
BlackBerryID <iddf1994one@gmail.com>
2022-05-16 17:32:04 +0300

    feat: add main page grid item

--- src/Modules/Pages/main/index.tsx
+++ src/Modules/Pages/main/index.tsx
@@ -1,6 +1,6 @@
 import { useAppDispatch, useAppSelector } from '../../../hooks/storeHooks';
 import { getBoards } from '../../../store/slices/boardListSlice';
-import { Paper, Grid } from '@mui/material';
+import { Card, CardMedia, Grid } from '@mui/material';
 import { useEffect } from 'react';
 
 import './main.scss';
@@ -18,8 +18,16 @@ export const Main = () => {
     <>
       <Grid className="main_container" container spacing={2}>
         {boardList.map((item, index) => (
-          <Grid item key={index}>
-            <Paper elevation={3}>{item.title}</Paper>
+          <Grid className="main_item-wrapper" item key={index} xl={3} lg={3} md={4} sm={6} xs={12}>
+            <Card className="main_item">
+              <CardMedia
+                className="main_item__image"
+                component="img"
+                image="https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80"
+                alt="card photo"
+              />
+              <h2 className="main_item__title">{item.title}</h2>
+            </Card>
           </Grid>
         ))}
       </Grid>
