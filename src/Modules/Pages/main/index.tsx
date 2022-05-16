871a3e6803adea2ad7403b5bdaf71e6deda9419f
BlackBerryID <iddf1994one@gmail.com>
2022-05-16 17:45:28 +0300

    feat: add openBoard method

--- src/Modules/Pages/main/index.tsx
+++ src/Modules/Pages/main/index.tsx
@@ -1,7 +1,10 @@
 import { useAppDispatch, useAppSelector } from '../../../hooks/storeHooks';
 import { getBoards } from '../../../store/slices/boardListSlice';
-import { Card, CardMedia, Grid } from '@mui/material';
+import { Card, CardMedia, CardContent, Typography, Grid } from '@mui/material';
 import { useEffect } from 'react';
+import { getCurrentBoard } from '../../../store/slices/currentBoardSlice';
+import { useNavigate } from 'react-router-dom';
+import { Paths } from '../../../constants';
 
 import './main.scss';
 
@@ -9,16 +12,32 @@ export const Main = () => {
   const { token } = useAppSelector((state) => state.signinSignup);
   const { boardList } = useAppSelector((state) => state.boardList);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getBoards({ token }));
   }, [dispatch, token]);
 
+  const openBoard = (boardId: string) => {
+    getCurrentBoard({ boardId, token });
+    navigate(Paths.board);
+  };
+
   return (
     <>
       <Grid className="main_container" container spacing={2}>
         {boardList.map((item, index) => (
-          <Grid className="main_item-wrapper" item key={index} xl={3} lg={3} md={4} sm={6} xs={12}>
+          <Grid
+            className="main_item-wrapper"
+            item
+            key={index}
+            xl={3}
+            lg={3}
+            md={4}
+            sm={6}
+            xs={12}
+            onClick={() => openBoard(item.id)}
+          >
             <Card className="main_item">
               <CardMedia
                 className="main_item__image"
@@ -26,7 +45,11 @@ export const Main = () => {
                 image="https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80"
                 alt="card photo"
               />
-              <h2 className="main_item__title">{item.title}</h2>
+              <CardContent className="main_item__title">
+                <Typography component="div" variant="h5">
+                  {item.title}
+                </Typography>
+              </CardContent>
             </Card>
           </Grid>
         ))}
