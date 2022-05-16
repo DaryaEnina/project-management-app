58584430b4d883583fb08307461f18c36031112b
BlackBerryID <iddf1994one@gmail.com>
2022-05-16 16:40:25 +0300

    feat: init main page grid container

--- src/Modules/Pages/main/index.tsx
+++ src/Modules/Pages/main/index.tsx
@@ -1,29 +1,28 @@
 import { useAppDispatch, useAppSelector } from '../../../hooks/storeHooks';
 import { getBoards } from '../../../store/slices/boardListSlice';
+import { Paper, Grid } from '@mui/material';
+import { useEffect } from 'react';
+
+import './main.scss';
 
 export const Main = () => {
   const { token } = useAppSelector((state) => state.signinSignup);
   const { boardList } = useAppSelector((state) => state.boardList);
   const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    dispatch(getBoards({ token }));
+  }, [dispatch, token]);
+
   return (
     <>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          fontSize: '40px',
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-        }}
-      >
-        <button onClick={() => dispatch(getBoards({ token }))}>Load boards</button>
+      <Grid className="main_container" container spacing={2}>
         {boardList.map((item, index) => (
-          <p key={index}>{item.title}</p>
+          <Grid item key={index}>
+            <Paper elevation={3}>{item.title}</Paper>
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </>
   );
 };
