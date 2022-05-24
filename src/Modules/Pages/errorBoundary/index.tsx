f7788ca0190401004db7780a089f2f3d2e69d311
BlackBerryID <iddf1994one@gmail.com>
2022-05-24 12:28:38 +0300

    feat: add three more list items of error boundary

--- src/Modules/Pages/errorBoundary/index.tsx
+++ src/Modules/Pages/errorBoundary/index.tsx
@@ -6,25 +6,26 @@ import { Paths } from '../../../constants';
 
 interface State {
   hasError: boolean;
+  error: string;
 }
 
 export class ErrorBoundary extends Component<errorBoundaryProps, State> {
   constructor(props: errorBoundaryProps) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: '' };
   }
 
   static getDerivedStateFromError(_: Error): State {
-    return { hasError: true };
+    return { hasError: true, error: _.message as string };
   }
 
   render() {
     if (this.state.hasError) {
       return (
         <Box className="error-boundary">
-          <h4>Что-то пошло не так</h4>
-          <h4>Но всё поправимо!</h4>
-          <h4>Следуйте инструкциям:</h4>
+          <h3>Что-то пошло не так</h3>
+          <h3>Но всё поправимо!</h3>
+          <h3>Следуйте инструкциям:</h3>
           <ol className="error-boundary_list">
             <li>
               Для начала нажмите клавишу F5 на своей клавиатуре. В случае, если ошибка не серьезна,
@@ -32,7 +33,7 @@ export class ErrorBoundary extends Component<errorBoundaryProps, State> {
             </li>
             <li>
               Если ошибка повторяется и нажатие F5 не помогает, попробуйте открыть другие страницы
-              сайта: например,{' '}
+              сайта и нажмите клавишу F5: например,{' '}
               <Button
                 className="error-boundary_btn"
                 variant="text"
@@ -42,7 +43,23 @@ export class ErrorBoundary extends Component<errorBoundaryProps, State> {
               </Button>
               .
             </li>
+            <li>
+              Если другие страницы сайта открываются, а недоступна лишь текущая, попробуйте
+              вернуться к ней через некоторое время. Возможно, на сайте ведутся профилактические
+              работы.
+            </li>
+            <li>
+              Если же ошибка на данной странице повторяется постоянно, напишите нам о ней с
+              подробным описанием: в каком случае проявляется, какой url страницы, какой текст
+              ошибки ({this.state.error}), на какую почту вы зарегистрированы на нашем сайте — на
+              адрес support@pma.by.
+            </li>
+            <li>
+              Если работа сайта нестабильна, недоступно большинство страниц, — значит, проблема
+              серьезная и с ней уже разбираются. Пожалуйста, запаситесь терпением.
+            </li>
           </ol>
+          <div>Спасибо, что выбираете нас!</div>
         </Box>
       );
     }
